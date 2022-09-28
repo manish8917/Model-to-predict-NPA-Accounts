781b15c5fdfd25ad1847ba929678c4b767cbb560
manish8917 <manishislampur1988@gmail.com>
2022-09-29 04:38:25 +0530

    Git Push

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Potential NPA Accounts Detection",
+    version="0.0.3",
+    description="ML project",
+    author="Manish Kumar",
+    packages=find_packages(),
+    license="MIT"
+)
